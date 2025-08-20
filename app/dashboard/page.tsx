44e49e026461259c53c5d8e47c3a9a4e4a72c553
Anish Baraily <an2001.baraily@gmail.com>
2025-08-20 18:51:53 +0545

    auth almost done

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function DashboardPage() {
+    return (
+        <div className="p-24">
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,13 +6,14 @@ import { Card } from "@/components/ui/card";
 import { Container } from "@/components/ui/container";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
-import { Lock, Mail } from "lucide-react";
+import { Lock, Mail, User } from "lucide-react";
 
 
 export default function LoginPage() {
 
     const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
+const [name, setName] = useState("");
 
 
     return (
@@ -21,10 +22,30 @@ const [password, setPassword] = useState("");
                 <Card className="w-full md:w-5/12 max-w-2xl p-8 md:p-10 rounded-3xl shadow-2xl border border-primary/10 bg-card/90 backdrop-blur-lg mt-12">
                     <div className="text-center mb-6">
                         <h1 className="text-2xl md:text-3xl font-extrabold bg-gradient-to-r from-primary to-primary/80 bg-clip-text text-transparent mb-1 tracking-tight">
-                            Sign In to uWise
+                            Sign Up
                         </h1>
-                        <p className="text-base text-muted-foreground font-medium">Welcome back! Please sign in to your account.</p>
+                        <p className="text-base text-muted-foreground font-medium">Please fill out the form below to create your account.</p>
                     </div>
+                <div className="space-y-3">
+                    <div>
+                        <label htmlFor="name"
+                        className="block text-base font-semibold mb-1">
+                            Name
+                        </label>
+                        <div className="relative">
+                            <User
+                            className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground"/>
+                            <Input
+                            id="name"
+                            type="name"
+                            placeholder="Enter your name"
+                            className="pl-12 py-2 text-base rounded-xl bg-card bg-opacity-80 border border-primary focus:ring-2 focus:ring-primary text-foreground placeholder:text-muited-foreground"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            required/>
+                        </div>
+                    </div>
+                </div>
                 <div className="space-y-3">
                     <div>
                         <label htmlFor="email"
@@ -49,7 +70,7 @@ const [password, setPassword] = useState("");
                     <div>
                         <label htmlFor="password"
                         className="block text-base font-semibold mb-1">
-                            Password
+                            Create a password
                         </label>
                         <div className="relative">
                             <Lock
@@ -70,7 +91,7 @@ const [password, setPassword] = useState("");
                         <Button type="submit" className="w-full py-2 text-base rounded-xl font-bold bg-gradient-to-r from-primary to-primary/80 shadow-md hover:from-primary/80 hover:to-primary"
                         size="lg"
                         typeof="button">
-                            Sign In
+                            Sign Up
                         </Button>
                     
                 </div>
@@ -78,9 +99,9 @@ const [password, setPassword] = useState("");
                     
                     <div className="flex items-center justify-center gap-2 text-sm">
 
-                       <span>Don&apos;t have an account?
+                       <span>Already have an account?
                        </span>
-                        <Link href="/signup" className="text-primary font-semibold hover:underline">Sign Up</Link>|
+                        <Link href="/login" className="text-primary font-semibold hover:underline">Sign In</Link>|
                        <Link href="/forgot-password" className="text-primary hover:underline font-semibold">Forgot password?</Link>
                     
                     </div>
