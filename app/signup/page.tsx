4129db0104112a95ddeedf0342158d66d17a6861
Anish Baraily <an2001.baraily@gmail.com>
2025-11-01 22:00:43 +0545

    home

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -17,33 +17,7 @@ const [name, setName] = useState("");
 
 
     return (
-        <div className="min-h-screen w-feull flex items-center justify-center bg-gradient-to-br from-primary/10 via-background to-secondary/30">
-            <Container className="flex flex-col items-center justify-center w-full">
-                <Card className="w-full md:w-5/12 max-w-2xl p-8 md:p-10 rounded-3xl shadow-2xl border border-primary/10 bg-card/90 backdrop-blur-lg mt-12">
-                    <div className="text-center mb-6">
-                        <h1 className="text-2xl md:text-3xl font-extrabold bg-gradient-to-r from-primary to-primary/80 bg-clip-text text-transparent mb-1 tracking-tight">
-                            Sign Up
-                        </h1>
-                        <p className="text-base text-muted-foreground font-medium">Please fill out the form below to create your account.</p>
-                    </div>
-                <div className="space-y-3">
-                    <div>
-                        <label htmlFor="name"
-                        className="block text-base font-semibold mb-1">
-                            Name
-                        </label>
-                        <div className="relative">
-                            <User
-                            className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground"/>
-                            <Input
-                            id="name"
-                            type="name"
-                            placeholder="Enter your name"
-                            className="pl-12 py-2 text-base rounded-xl bg-card bg-opacity-80 border border-primary focus:ring-2 focus:ring-primary text-foreground placeholder:text-muited-foreground"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            required/>
-                        </div>
+       
                     </div>
                 </div>
                 <div className="space-y-3">
