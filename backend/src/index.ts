05e79fc1bd416c1101ad7b9b949ad7f4c1abed7e
Anish Baraily <an2001.baraily@gmail.com>
2026-04-10 21:54:29 +0545

    setup db conn

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,42 @@
+import 'dotenv/config';
 import express ,{Request,Response} from 'express';
+import {serve} from "inngest/express";
+import {inngest } from './inngest'
+import {functions as inngestFunctions } from './inngest/functions'
+import {logger} from "./utils/logger"
 
 const app = express();
 
-const PORT = 3001;
+// const PORT = 3001;
 
 app.use(express.json())
+app.use("/api/inngest",serve({client:inngest,functions:inngestFunctions}))
 
 app.get('/',(req:Request,res:Response)=>{
     res.send("Hello!")
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server started at port ${PORT}`)
+app.get('/api/chat',(req: Request, res: Response)=>{
+    res.send("Hi, how may i help you today?")
 })
+
+// app.listen(PORT,()=>{
+//     console.log(`Server started at port ${PORT}`)
+// })
+
+const startServer = async ()=>{
+    try {
+        const PORT = process.env.PORT || 3001;
+        app.listen(PORT,()=>{
+            logger.info(`Server is running on port ${PORT}`);
+            logger.info(
+                `Inngest endpoint available at http://localhost:${PORT}/api/inngest`
+            )
+        })
+    } catch (error) {
+        logger.error("Failed to start server:",error)
+        process.exit(1)
+    }
+}
+
+startServer();
